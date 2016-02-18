a9d4dabfa64a52f8c5b99f41fa30d5e7bc7d0b2c
James Turello <jturello@yahoo.com>
2016-02-18 12:08:58 -0800

    add mCreatedAt, mCompleted, mId member variable to Task class with corresponding unit tests

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,11 +1,50 @@
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+
+
 public class Task {
-  String mDescription;
+  private static ArrayList<Task> instances = new ArrayList<Task>();
+
+  private String mDescription;
+  private LocalDateTime mCreatedAt;
+  private boolean mCompleted;
+  private int mId;
+
 
   public Task(String description) {
     mDescription = description;
+    mCreatedAt = LocalDateTime.now();
+    mCompleted = false;
+    instances.add(this);
+    mId = instances.size();
   }
-  
+
   public String getDescription() {
     return mDescription;
   }
+
+  public LocalDateTime getCreateAt() {
+    return mCreatedAt;
+  }
+
+  public static ArrayList<Task> all() {
+    return instances;
+  }
+
+  public int getId() {
+    return mId;
+  }
+
+  public static Task find(int id) {
+    try {
+          return instances.get(id - 1);
+    } catch(IndexOutOfBoundsException e) {
+      return null;
+    }
+  }
+
+  public static void clear() {
+    instances.clear();
+  }
+
 }
--- src/test/java/TaskTest.java
+++ src/test/java/TaskTest.java
@@ -1,3 +1,5 @@
+import java.time.LocalDateTime;
+
 import org.junit.*;
 import static org.junit.Assert.*;
 
@@ -14,4 +16,44 @@ public class TaskTest {
     Task myTask = new Task("Mow the lawn");
     assertEquals("Mow the lawn", myTask.getDescription());
   }
+
+  @Test
+  public void getCreateAt_instantiatesWithCurrentTime_today() {
+    Task myTask = new Task("Mow the lawn");
+  assertEquals(LocalDateTime.now().getDayOfWeek(), myTask.getCreateAt().getDayOfWeek());
+  }
+
+  @Test
+  public void all_returnsAllInstancesOfTask_true() {
+    Task firstTask = new Task("Mow the lawn");
+    Task secondTask = new Task("Buy groceries");
+    assertTrue(Task.all().contains(firstTask));
+    assertTrue(Task.all().contains(secondTask));
+  }
+
+  @Test
+  public void newId_tasksInstantiatedWithAnID_true() {
+    Task myTask = new Task("Mow the lawn");
+    assertEquals(Task.all().size(), myTask.getId());
+  }
+
+  @Test
+  public void find_returnsTaskWithSameId_secondTask() {
+    Task firstTask = new Task("Mow the lawn");
+    Task secondTask = new Task("Buy groceries");
+    assertEquals(Task.find(secondTask.getId()), secondTask);
+  }
+
+  @Test
+  public void find_returnsNullWhenNoTaskFound_true() {
+    assertTrue(Task.find(999) == null);
+  }
+
+  @Test
+  public void clear_emptiesAllTasksFromArrayList() {
+    Task myTask = new Task("Mow the lawn");
+    Task.clear();
+    assertEquals(Task.all().size(), 0);
+  }
+
 }
